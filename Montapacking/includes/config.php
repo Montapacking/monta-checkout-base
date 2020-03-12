0ca80a8d93445ba370f0d492a60dcea89bfeb910
Andre Vlot <andrevlot@gmail.com>
2020-03-12 11:54:24 +0100

    dev

--- Montapacking/includes/config.php
+++ Montapacking/includes/config.php
@@ -1,7 +1,7 @@
 <?php
     define('BASE_DOMAIN', 'http://localhost:7000');
-    define('API_USER', 'andre');
-    define('API_PASSWORD', 'EDbssPv*Xy3ZvY&#');
-    define('API_SHOP', 'Demoshop.nl');
+    define('API_USER', '');
+    define('API_PASSWORD', '');
+    define('API_SHOP', '');
     define('API_LANGUAGE', 'NL');
-    define('GOOGLE_API_KEY', 'AIzaSyDuNPZlLCSlPM6Z55EUhanhzLZSiKcoUi0');
+    define('GOOGLE_API_KEY', '');
